2f5a528ff92b84cbbd2ce50ec98ffc20c835609b
Edward Higgins <ed.higgins@york.ac.uk>
2024-02-21 03:32:59 +0000

    Buttons and State

--- demo_app/lib/homepage.dart
+++ demo_app/lib/homepage.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+
   const Homepage({super.key});
 
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  String _name = "mate";
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column (
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text( 'Hello, world!', style: TextStyle(fontSize: 36)),
-          Text( 'How are you?', style: TextStyle(fontSize: 24)),
+          Text( 'Hello, ${_name}!', style: TextStyle(fontSize: 36)),
+
+          SizedBox(height: 20),
+
+          ElevatedButton(
+            onPressed: () {
+              setState(() { 
+                if (_name != "Ed") {
+                  _name = "Ed";
+                } else {
+                  _name = "again";
+                }
+              });
+            },
+            child: Text("My name is Ed!")
+          ),
+
         ],
       ),
     );
